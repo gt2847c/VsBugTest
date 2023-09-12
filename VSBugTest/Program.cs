bd3f2ffbd937e15c111df73cd56efd3237b4bb5b
David A. Taylor <david@the-taylor-family.org>
2023-09-12 14:21:48 -0400

    Added code to demonstrate debugger breaking code

--- VSBugTest/Program.cs
+++ VSBugTest/Program.cs
@@ -17,6 +17,20 @@ namespace VSBugTest
                 }
             }
 
+            /*
+            // This section does not have using() so won't dispose.  If you stop after records1 at the ;
+            // and view the Locals entry - expand the records1.Results View and scroll until the results 
+            // disappear, then step to the ToList(), "list" will be empty.  
+            // Alternatively if you don't get the results to disappear, then ToList will generate 4,105 entries 
+
+            var reader1 = new StreamReader(_sourcefile);
+            var csv1 = new CsvReader(reader1, CultureInfo.InvariantCulture);
+            var records1 = csv1.GetRecords<csvdto>();
+            ;
+            var list = records1.ToList();
+            ;
+            */
+
 
         }
     }
