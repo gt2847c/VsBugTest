5ce54eeec227cb77221d1e4963548c414c1aabad
David A. Taylor <david@the-taylor-family.org>
2023-09-12 13:31:30 -0400

    VS2022 Bug demonstration

---
+++ VSBugTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace VSBugTest
+{
+    public class Program
+    {
+        private const string _sourcefile = "datademo.csv";
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(_sourcefile))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<csvdto>();
+                    ;
+                }
+            }
+
+
+        }
+    }
+}
